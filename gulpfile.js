cad2a2d71d4ddd6b52da4211295189788a0f65c3
Aleksander Shumeiko <music.dll@mail.ru>
2020-07-24 13:33:05 +0300

    Added price table screen

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,140 @@
-"use strict";
+'use strict';
+var gulp = require('gulp'), //есть
 
-const gulp = require("gulp");
-const webpack = require("webpack-stream");
-const browsersync = require("browser-sync");
+	concat = require('gulp-concat'), //есть
+	rename = require('gulp-rename'), //есть
+	del = require('del'), //есть
+	cache = require('gulp-cache'), //есть
 
-const dist = "./dist/";
+	uglify = require('gulp-uglifyjs'), //есть   
+	rigger = require('gulp-rigger'), //нет    
+	sass = require('gulp-sass'), //есть
+	sourcemaps = require('gulp-sourcemaps'), //нет
+	//cssmin      = require('gulp-minify-css'),      //есть   старый
+	cssnano = require('gulp-cssnano'), //есть
+	imagemin = require('gulp-imagemin'), //есть
+	pngquant = require('imagemin-pngquant'), //есть   
+	//spritesmith = require('gulp.spritesmith'),   //нет
+	plumber = require('gulp-plumber'), //есть
+	autoprefixer = require('gulp-autoprefixer'),
+	copy = require('gulp-contrib-copy'),
+	browserSync = require('browser-sync');
 
-gulp.task("copy-html", () => {
-    return gulp.src("./src/index.html")
-                .pipe(gulp.dest(dist))
-                .pipe(browsersync.stream());
+let path = {
+	js: ['src/js'],
+	js_build: 'dist/js',
+
+	scripts: ['src/js/**/*.js'],
+	scripts_build: '',
+
+	css: ['src/assets/sass/styles.sass', '!node_modules/**/*.sass', 'gulp/**/*.sass'],
+	css_build: ['dist/assets/css/'],
+
+	img: 'src/assets/img/**/*',
+	img_build: 'dist/assets/img',
+
+	html: 'src/**/*.html',
+	html_build: 'dist',
+
+	fonts: 'src/assets/fonts/**/*',
+	fonts_build: 'dist/assets/fonts'
+};
+
+// Local Server
+gulp.task('browser-sync', function () {
+	browserSync({
+		server: {
+			baseDir: 'dist'
+		},
+		notify: false,
+		// proxy: "prostitutkikieva.test/",
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	});
 });
 
-gulp.task("build-js", () => {
-    return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist))
-                .on("end", browsersync.reload);
+gulp.task('code:build', function (done) {
+	gulp.src(path.html)
+		.pipe(gulp.dest(path.html_build))
+		.pipe(browserSync.stream());
+
+	done();
 });
 
-gulp.task("copy-assets", () => {
-    return gulp.src("./src/assets/**/*.*")
-                .pipe(gulp.dest(dist + "/assets"))
-                .on("end", browsersync.reload);
+gulp.task('fonts:copy', function (done) {
+	gulp.src(path.fonts)
+		.pipe(gulp.dest(path.fonts_build))
+		.pipe(browserSync.stream());
+
+	done();
 });
 
-gulp.task("watch", () => {
-    browsersync.init({
-		server: "./dist/",
-		port: 4000,
-		notify: true
-    });
-    
-    gulp.watch("./src/index.html", gulp.parallel("copy-html"));
-    gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
-    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+gulp.task('js:build', function (done) {
+	gulp.src(path.js) // Найдем наш main файл         
+		.pipe(plumber())
+		.pipe(rigger()) // Прогоним через rigger
+		.pipe(concat('libs.min.js'))
+		.pipe(sourcemaps.init()) // Инициализируем sourcemap
+		.pipe(uglify()) // Сожмем наш js
+		.pipe(sourcemaps.write()) // Пропишем карты
+		.pipe(plumber.stop())
+		.pipe(gulp.dest(path.js_build)); // Выплюнем готовый файл в build
+
+	done();
 });
 
-gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js"));
-
-gulp.task("build-prod-js", () => {
-    return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'production',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist));
+gulp.task('scripts', function (done) {
+	return gulp.src(path.scripts)
+		.pipe(plumber())
+		.pipe(rigger()) // Прогоним через rigger
+		.pipe(uglify()) // Сожмем наш js
+		.pipe(plumber.stop())
+		.pipe(gulp.dest(path.scripts_build)); // Выплюнем готовый файл в build
 });
 
-gulp.task("default", gulp.parallel("watch", "build"));
+
+gulp.task('css:build', function (done) {
+	gulp.src(path.css) // Выберем наш main.scss         
+		.pipe(plumber())
+		.pipe(sourcemaps.init()) // То же самое что и с js
+		.pipe(sass()) // Скомпилируем         
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+			cascade: true
+		}))
+		.pipe(cssnano()) // Сожмем         
+		.pipe(sourcemaps.write()) // Пропишем карты
+		.pipe(plumber.stop())
+		.pipe(gulp.dest(path.css_build)) // И в build
+		.pipe(browserSync.stream());
+
+	done();
+});
+
+
+gulp.task('img:build', function () {
+	return gulp.src(path.img) // Берем все изображения из app
+		.pipe(cache(imagemin({ // С кешированием
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{
+				removeViewBox: false
+			}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest(path.img_build)); // Выгружаем на продакшен
+});
+
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+});
+
+gulp.task('watch', function () {
+	gulp.watch(path.css, gulp.parallel('css:build'));
+	gulp.watch(path.img, gulp.parallel('img:build'));
+	gulp.watch(path.js, gulp.parallel('js:build'));
+	gulp.watch(path.html, gulp.parallel('code:build'));
+	gulp.watch(path.html, gulp.parallel('fonts:copy'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'css:build', 'code:build', 'img:build', 'js:build', 'fonts:copy', 'browser-sync'));
